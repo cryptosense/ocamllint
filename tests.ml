a231dd1210cb11d8034f97ce41dac2a04cd65586
Etienne Millon <etienne@cryptosense.com>
2015-12-18 17:35:29 +0100

    Add test for style rules

--- tests.ml
+++ tests.ml
@@ -1,13 +1,12 @@
-open OUnit
+open OUnit2
 
 let test_snake_case =
-  let open Rules in
   let t (s, r) =
     let name =
       Printf.sprintf "is_snake_case %S?" s
     in
     name >:: fun ctxt ->
-      assert_equal r (is_snake_case s)
+      assert_equal r (Rules.is_snake_case s)
   in
   List.map t
     [ "ident", true
@@ -15,9 +14,34 @@ let test_snake_case =
     ; "someIdent", false
     ]
 
+let expr_to_string expr =
+  let b = Buffer.create 0 in
+  let fmt = Format.formatter_of_buffer b in
+  Printast.expression 0 fmt expr;
+  Format.pp_print_flush fmt ();
+  Buffer.contents b
+
+let test_style =
+  let t (expr, r) =
+    let name =
+      Printf.sprintf "Rate %s" (expr_to_string expr)
+    in
+    name >:: fun ctxt ->
+      assert_equal
+        ~ctxt
+        ~cmp:[%eq:string option]
+        ~printer:[%show:string option]
+        r
+        (Rules.rate_expression expr)
+  in
+  List.map t
+    [ [%expr List.map f [2] ], Some "List.map on singleton"
+    ]
+
 let suite =
   "Tests" >:::
   [ "Snake case" >::: test_snake_case
+  ; "Style" >::: test_style
   ]
 
 let _ =
